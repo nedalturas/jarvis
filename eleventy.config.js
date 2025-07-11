ed3e79f989c96ae2e931792cbeb79889958e3879
Ned <nedalturas527@gmail.com>
2025-07-11 12:13:08 +0400

    added a js folder and updated the config

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,7 @@
 module.exports = function(eleventyConfig){
     eleventyConfig.addPassthroughCopy("css");
-
+    eleventyConfig.addPassthroughCopy("js");
+    
     return{
         dir: {
             input: ".",
---
+++ js/script.js
@@ -0,0 +1,1 @@
+console.log('Hello fucking world');
